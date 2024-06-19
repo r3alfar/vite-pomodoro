0d6a665339429e69b6d55605d04c0a81619f9384
r3alfar <farreladivia@gmail.com>
2024-06-19 18:25:49 +0700

    add progress comp

--- src/components/pomodoro/pomo-selector.tsx
+++ src/components/pomodoro/pomo-selector.tsx
@@ -1,11 +1,17 @@
 import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+
+// import { Flat } from '@alptugidin/react-circular-progress-bar'
 
 
 
 function PomoSelector() {
   return (
-    <div>
+    <div
+    // className="relative z-20"
+    >
       <div className="space-y-1">
         <h4 className="text-sm font-medium leading-none">Radix Primitives</h4>
         <p className="text-sm text-muted-foreground">
@@ -34,6 +40,15 @@ function PomoSelector() {
           </Button>
         </div>
       </div>
+
+      <div
+        className="grid place-items-center max-h-[300px] max-w-[300px] h-[300px] w-[300px] mx-auto mt-8"
+      >
+        <CircularProgressbar value={62} text={`${62}%`} />
+      </div>
+
+
+
     </div>
   )
 }
